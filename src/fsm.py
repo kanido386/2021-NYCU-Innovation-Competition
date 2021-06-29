6fb4d6f911f395aa0ebf514031257673dd45f9c5
kanido386 <kanido386@gmail.com>
2021-06-29 10:41:57 +0800

    Add menu

--- src/fsm.py
+++ src/fsm.py
@@ -5,139 +5,148 @@ from service.basic import send_text_message, push_text_message
 from service.other import send_youtube_video
 from service.blockchain import users, service_tokens
 from service.firebase import write_message, read_message, upload_image, send_image
+from service.hardcode import send_menu
 
 class TocMachine(GraphMachine):
   def __init__(self, **machine_configs):
     self.machine = GraphMachine(model=self, **machine_configs)
+
+
+  def is_going_to_menu(self, event):
+    text = event.message.text
+    return "哈囉" in text
   
   
-  def is_going_to_state1(self, event):
-    text = event.message.text
-    return text.lower() == "go to state1"
+  # def is_going_to_state1(self, event):
+  #   text = event.message.text
+  #   return text.lower() == "go to state1"
 
 
-  def is_going_to_state2(self, event):
-    text = event.message.text
-    return text.lower() == "go to state2"
+  # def is_going_to_state2(self, event):
+  #   text = event.message.text
+  #   return text.lower() == "go to state2"
 
 
-  def is_going_to_youtube(self, event):
-    text = event.message.text
-    return "聽" in text
+  # def is_going_to_youtube(self, event):
+  #   text = event.message.text
+  #   return "聽" in text
 
 
-  def is_going_to_try_blockchain(self, event):
-    text = event.message.text
-    return "Give me" in text
+  # def is_going_to_try_blockchain(self, event):
+  #   text = event.message.text
+  #   return "Give me" in text
 
-  def is_going_to_write_message(self, event):
-    text = event.message.text
-    return "Message" in text and len(text) > 7
+  # def is_going_to_write_message(self, event):
+  #   text = event.message.text
+  #   return "Message" in text and len(text) > 7
 
-  def is_going_to_read_message(self, event):
-    text = event.message.text
-    return text.lower() == "message"
+  # def is_going_to_read_message(self, event):
+  #   text = event.message.text
+  #   return text.lower() == "message"
 
-  def is_going_to_image(self, event):
-    print(event)
-    return event.message.type == "image"
+  # def is_going_to_image(self, event):
+  #   print(event)
+  #   return event.message.type == "image"
 
-  def is_going_to_see_image(self, event):
-    print(event)
-    text = event.message.text
-    return text.lower() == "see image"
+  # def is_going_to_see_image(self, event):
+  #   print(event)
+  #   text = event.message.text
+  #   return text.lower() == "see image"
 
-  
-  def on_enter_user(self, event):
-    user_id = event.source.user_id
-    push_text_message(user_id, '你好啊～')
+  # ==================================================
 
-
-  def on_enter_state1(self, event):
-    print("I'm entering state1")
-
-    reply_token = event.reply_token
-    send_text_message(reply_token, "Trigger state1")
-    self.go_back()
-
-
-  # def on_exit_state1(self):
-  #   print("Leaving state1")
-
-
-  def on_enter_state2(self, event):
-    print("I'm entering state2")
-
-    reply_token = event.reply_token
-    send_text_message(reply_token, "Trigger state2")
-    self.go_back()
-
-
-  # def on_exit_state2(self):
-  #   print("Leaving state2")
-
-
-  def on_enter_youtube(self, event):
-    print("I'm entering youtube")
-
-    id = event.source.user_id
-    reply_token = event.reply_token
-    query = event.message.text
-    send_youtube_video(id, reply_token, query)
-    self.go_back()
-
-
-  def on_enter_try_blockchain(self, event):
-    print("I'm entering try_blockchain")
+  def on_enter_menu(self, event):
+    print("I'm entering menu")
 
     user_id = event.source.user_id
-    reply_token = event.reply_token
-    amount = int(event.message.text[7:])
-    user_wallet_address = users.retrieve.wallet_address(user_id)
-    service_tokens.mint(user_wallet_address, amount)
-    send_text_message(reply_token, f"恭喜獲得 {amount} 枚健康幣！")
+    send_menu(user_id)
     self.go_back()
 
+  # def on_enter_state1(self, event):
+  #   print("I'm entering state1")
 
-  def on_enter_write_message(self, event):
-    print("I'm entering write_message")
-
-    user_id = event.source.user_id
-    message = event.message.text[8:]
-    write_message(user_id, message)
-    reply_token = event.reply_token
-    send_text_message(reply_token, "訊息存起來囉！")
-    self.go_back()
+  #   reply_token = event.reply_token
+  #   send_text_message(reply_token, "Trigger state1")
+  #   self.go_back()
 
 
-  def on_enter_read_message(self, event):
-    print("I'm entering read_message")
-
-    user_id = event.source.user_id
-    message = read_message(user_id)
-    reply_token = event.reply_token
-    send_text_message(reply_token, f"這是剛剛存起來的訊息：\n\n{message}")
-    self.go_back()
+  # # def on_exit_state1(self):
+  # #   print("Leaving state1")
 
 
-  def on_enter_image(self, event):
-    print("I'm entering image")
+  # def on_enter_state2(self, event):
+  #   print("I'm entering state2")
 
-    user_id = event.source.user_id
-    message_id = event.message.id
-    file_name = str(event.timestamp) + ".jpg"
-    upload_image(user_id, message_id, file_name)
-
-    reply_token = event.reply_token
-    send_text_message(reply_token, "照片已上傳。")
-    self.go_back()
+  #   reply_token = event.reply_token
+  #   send_text_message(reply_token, "Trigger state2")
+  #   self.go_back()
 
 
-  def on_enter_see_image(self, event):
-    print("I'm entering see_image")
+  # # def on_exit_state2(self):
+  # #   print("Leaving state2")
 
-    user_id = event.source.user_id
-    reply_token = event.reply_token
-    send_image(user_id, reply_token)
+
+  # def on_enter_youtube(self, event):
+  #   print("I'm entering youtube")
+
+  #   id = event.source.user_id
+  #   reply_token = event.reply_token
+  #   query = event.message.text
+  #   send_youtube_video(id, reply_token, query)
+  #   self.go_back()
+
+
+  # def on_enter_try_blockchain(self, event):
+  #   print("I'm entering try_blockchain")
+
+  #   user_id = event.source.user_id
+  #   reply_token = event.reply_token
+  #   amount = int(event.message.text[7:])
+  #   user_wallet_address = users.retrieve.wallet_address(user_id)
+  #   service_tokens.mint(user_wallet_address, amount)
+  #   send_text_message(reply_token, f"恭喜獲得 {amount} 枚健康幣！")
+  #   self.go_back()
+
+
+  # def on_enter_write_message(self, event):
+  #   print("I'm entering write_message")
+
+  #   user_id = event.source.user_id
+  #   message = event.message.text[8:]
+  #   write_message(user_id, message)
+  #   reply_token = event.reply_token
+  #   send_text_message(reply_token, "訊息存起來囉！")
+  #   self.go_back()
+
+
+  # def on_enter_read_message(self, event):
+  #   print("I'm entering read_message")
+
+  #   user_id = event.source.user_id
+  #   message = read_message(user_id)
+  #   reply_token = event.reply_token
+  #   send_text_message(reply_token, f"這是剛剛存起來的訊息：\n\n{message}")
+  #   self.go_back()
+
+
+  # def on_enter_image(self, event):
+  #   print("I'm entering image")
+
+  #   user_id = event.source.user_id
+  #   message_id = event.message.id
+  #   file_name = str(event.timestamp) + ".jpg"
+  #   upload_image(user_id, message_id, file_name)
+
+  #   reply_token = event.reply_token
+  #   send_text_message(reply_token, "照片已上傳。")
+  #   self.go_back()
+
+
+  # def on_enter_see_image(self, event):
+  #   print("I'm entering see_image")
+
+  #   user_id = event.source.user_id
+  #   reply_token = event.reply_token
+  #   send_image(user_id, reply_token)
     
-    self.go_back()
+  #   self.go_back()
--- src/machine.py
+++ src/machine.py
@@ -3,61 +3,69 @@ from fsm import TocMachine
 def create_machine():
   machine = TocMachine(
     # TODO: 用 append 的方式來添加
-    states=["user", "see_image", "state1", "state2", "youtube", "try_blockchain", "write_message", "read_message", "image"],
+    states=["user", "menu"],
+    # states=["user", "menu", "see_image", "state1", "state2", "youtube", "try_blockchain", "write_message", "read_message", "image"],
     # TODO: 用 append 的方式來添加
     # TODO: message type other than text should put forward
     transitions=[
       {
           "trigger": "advance",
           "source": "user",
-          "dest": "image",
-          "conditions": "is_going_to_image",
+          "dest": "menu",
+          "conditions": "is_going_to_menu",
       },
-      {
-          "trigger": "advance",
-          "source": "user",
-          "dest": "see_image",
-          "conditions": "is_going_to_see_image",
-      },
-      {
-          "trigger": "advance",
-          "source": "user",
-          "dest": "state1",
-          "conditions": "is_going_to_state1",
-      },
-      {
-          "trigger": "advance",
-          "source": "user",
-          "dest": "state2",
-          "conditions": "is_going_to_state2",
-      },
-      {
-          "trigger": "advance",
-          "source": "user",
-          "dest": "youtube",
-          "conditions": "is_going_to_youtube",
-      },
-      {
-          "trigger": "advance",
-          "source": "user",
-          "dest": "try_blockchain",
-          "conditions": "is_going_to_try_blockchain",
-      },
-      {
-          "trigger": "advance",
-          "source": "user",
-          "dest": "write_message",
-          "conditions": "is_going_to_write_message",
-      },
-      {
-          "trigger": "advance",
-          "source": "user",
-          "dest": "read_message",
-          "conditions": "is_going_to_read_message",
-      },
+      # {
+      #     "trigger": "advance",
+      #     "source": "user",
+      #     "dest": "image",
+      #     "conditions": "is_going_to_image",
+      # },
+      # {
+      #     "trigger": "advance",
+      #     "source": "user",
+      #     "dest": "see_image",
+      #     "conditions": "is_going_to_see_image",
+      # },
+      # {
+      #     "trigger": "advance",
+      #     "source": "user",
+      #     "dest": "state1",
+      #     "conditions": "is_going_to_state1",
+      # },
+      # {
+      #     "trigger": "advance",
+      #     "source": "user",
+      #     "dest": "state2",
+      #     "conditions": "is_going_to_state2",
+      # },
+      # {
+      #     "trigger": "advance",
+      #     "source": "user",
+      #     "dest": "youtube",
+      #     "conditions": "is_going_to_youtube",
+      # },
+      # {
+      #     "trigger": "advance",
+      #     "source": "user",
+      #     "dest": "try_blockchain",
+      #     "conditions": "is_going_to_try_blockchain",
+      # },
+      # {
+      #     "trigger": "advance",
+      #     "source": "user",
+      #     "dest": "write_message",
+      #     "conditions": "is_going_to_write_message",
+      # },
+      # {
+      #     "trigger": "advance",
+      #     "source": "user",
+      #     "dest": "read_message",
+      #     "conditions": "is_going_to_read_message",
+      # },
       # TODO: 用 append 的方式來添加
       # TODO: message type other than text should put forward
-      {"trigger": "go_back", "source": ["see_image", "state1", "state2", "youtube", "try_blockchain", "write_message", "read_message", "image"], "dest": "user"},
+      {"trigger": "go_back", "source": ["menu"], "dest": "user"},
+      # {"trigger": "go_back", "source": ["menu", "see_image", "state1", "state2", "youtube", "try_blockchain", "write_message", "read_message", "image"], "dest": "user"},
     ],
     initial="user",
     auto_transitions=False,
