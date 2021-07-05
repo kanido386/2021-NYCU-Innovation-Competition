5427c211f39c54cbea45af42b8891ca1fd2578ac
kanido386 <62498455+kanido386@users.noreply.github.com>
2021-07-05 17:54:56 +0800

    Fix a very small bug... (#9)
    * Chore: find a bug after hackathon

--- src/service/blockchain/functionality.py
+++ src/service/blockchain/functionality.py
@@ -7,5 +7,5 @@ def init_blockchain(user_id):
   if address == None:
     amount = 30
     push_text_message(user_id, f'送您 {amount} 枚健康幣當作見面禮～\n快去 BITMAX Wallet 查收吧  \U0001f603')
-    res = users.transfer.service_token(users, amount)
+    res = users.transfer.service_token(user_id, amount)
     print(res)
